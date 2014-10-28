b8868e5d7b74a3982fadc27517985823b4975838
BluSoft <bartosz.szczepanski@blu-soft.pl>
2014-10-28 21:47:18 +0100

    Vars again.

--- app/mailers/mandrill_mailer.rb
+++ app/mailers/mandrill_mailer.rb
@@ -11,7 +11,7 @@ class MandrillMailer
   def send_confirmation(participant)
     prepare_message(participant)
     @mailer.messages.send_template(
-        'Confirmation',
+        'Confirmation_new',
         [{
             :name => 'main',
             :content => "empty"
