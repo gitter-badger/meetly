6781c8c3f253080f66edb7b6a2c36cf4a52f3fa6
BluSoft <bartosz.szczepanski@blu-soft.pl>
2014-10-28 21:05:09 +0100

    Modify view.

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -11,11 +11,10 @@ class ParticipantsController < ApplicationController
   def receive_form
     @participant = create_participant
     @mandrill = MandrillMailer.new
-    @mandrill.send_confirmation(@participant)
 
     respond_to do |format|
       if @participant.save
-
+        @mandrill.send_confirmation(@participant)
         format.json {render :json => @participant.to_json, :callback => params['callback']}
         format.html {render }
       end
@@ -28,6 +27,7 @@ class ParticipantsController < ApplicationController
     parameters = params.permit(:name, :surname, :age, :city, :email, :phone, :nights, :dinners, :gender)
     participant = Participant.new(parameters)
     participant.role = Role.find_by(name: 'Uczestnik')
+    participant.cost = calculate_price
     days = []
     days.push(Day.find_by_number(1)) if params[:day1]
     days.push(Day.find_by_number(2)) if params[:day2]
