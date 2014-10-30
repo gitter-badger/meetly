d88e34b2e40d73d26cac1e3e30585a3c9abfacb3
BluSoft <bartosz.szczepanski@blu-soft.pl>
2014-10-30 21:41:31 +0100

    remove duplicate migration.

--- db/migrate/20141026234311_add_timestamps_to_participants.rb
+++ db/migrate/20141026234311_add_timestamps_to_participants.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToParticipants < ActiveRecord::Migration
-  def change
-    add_column :participants, :created_at, :datetime
-    add_column :participants, :updated_at, :datetime
-  end
+#  def change
+ #   add_column :participants, :created_at, :datetime
+  #  add_column :participants, :updated_at, :datetime
+  #end
 end
