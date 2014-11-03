6985dc73511e4db7825179a551957c833498ccef
BluSoft <bartosz.szczepanski@blu-soft.pl>
2014-11-03 19:14:02 +0100

    Bug fix.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,17 +2,15 @@
 
 class RolesController < ApplicationController
 
+  respond_to :json, :js
+
   def get_role_price_table
     @role = Role.find_by(name: query_parameters[:name])
     if @role
       price_table = @role.price_table
-      respond_to do |format|
-        format.json{render :json => price_table.to_json, :status => 200, :callback => params['callback']}
-      end
+      respond_with price_table.to_json, :status => 200, :callback => params['callback'], content_type: "application/javascript"
     else
-      respond_to do |format|
-        format.json{render :json => nil, :status => 422}
-      end
+      respond_with nil, :status => 422, content_type: "application/javascript"
     end
   end
 
