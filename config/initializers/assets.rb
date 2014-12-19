76abcb958cc5ed76cddb5cdcf30c2e0012a22daf
Bartek Szczepanski <bartosz.szczepanski@blu-soft.pl>
2014-12-19 01:41:55 +0100

    combine with css-wizard work.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,18 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( jquery.selectbox-0.2.min.js )
+Rails.application.config.assets.precompile += %w( jquery.dataTables.js )
+Rails.application.config.assets.precompile += %w( dataTables.colVis.js )
+Rails.application.config.assets.precompile += %w( jquery.page-ui.js )
+Rails.application.config.assets.precompile += %w( data_table_config.js )
+Rails.application.config.assets.precompile += %w( normalize.css )
+Rails.application.config.assets.precompile += %w( jquery.dataTables.css )
+Rails.application.config.assets.precompile += %w( jquery.selectbox.css )
+Rails.application.config.assets.precompile += %w( dataTables.colVis.css )
+Rails.application.config.assets.precompile += %w( style.css.scss )
+Rails.application.config.assets.precompile += %w( fonts/fonts.css )
+Rails.application.config.assets.precompile += %w( style.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
