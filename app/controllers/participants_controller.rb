e74e01e003fd7d8c4821f18b44b06da467185ffe
Bartek Szczepanski <bartosz.szczepanski@blu-soft.pl>
2014-12-31 12:04:41 +0100

    .

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -95,10 +95,11 @@ class ParticipantsController < ApplicationController
 
   def summary
     @participants = Participant.includes(:days).includes(:role).all
-    @vol = Participant.all.inject(0) do |sum, e| if ((e.cost - e.paid)<0)
-                sum -= (e.cost - e.paid)
-                  end
+    @vol = Participant.all.inject(0) do |sum, e| 
+                if (e.cost - e.paid) < 0 
+                  sum -= (e.cost - e.paid)
                 end
+              end
   end
 
   def index
