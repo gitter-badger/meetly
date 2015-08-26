22299ae9a0d84c3703feb29e8c8aa1c5d4402e14
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-08-27 00:35:39 +0200

    schema.rb changed by migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227233149) do
+ActiveRecord::Schema.define(version: 20150826222410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,10 +103,10 @@ ActiveRecord::Schema.define(version: 20141227233149) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password"
+    t.string   "password_digest"
+    t.string   "email"
   end
 
 end
