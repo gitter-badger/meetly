b6c3dedf147bff77e11463f2902432ce5e1ab295
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-08-27 00:36:51 +0200

    Refactor User Model and Users Controller to use has_secure_password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save ? sign_in(@user) : render(:new)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password
+    )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessor :password
+  validates :name, uniqueness: true, length: { in: 3..20 }
+  validates :password, length: { in: 8..20 }, on: :create
+  validates :password, :name, :email, presence: true
 
-  before_save :encrypt_password
-  after_save :clear_password
+  has_secure_password
 
-  validates :name, presence: true, uniqueness: true, length: { in: 3..20 }
-  validates_length_of :password, in: 8..20, on: :create
-  validates :password, presence: true
-
-  def encrypt_password
-    self.salt = BCrypt::Engine.generate_salt
-    puts 'salt created!'
-    puts "#{salt}"
-
-    self.encrypted_password = BCrypt::Engine.hash_secret(password, self.salt)
-    puts 'encryption created!'
-    puts "#{encrypted_password}"
-  end
-
-  def clear_password
-    self.password = 'cleared!'
-    puts 'pswd cleared!'
-  end
-
-  def self.authenticate(username, login_password)
-    user = Account.find_by(name: username)
-    user.authenticate(login_password) if user
+  def self.authenticate(username, password)
+    user = User.find_by(name: username)
+    user.authenticate(password) if user
   end
 end
