76c9f813078f871ab47e95d38148ee1aa5d8931d
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-08-27 10:19:21 +0200

    Refactor Sessions Controller and it's views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,14 @@
 class SessionsController < ApplicationController
-  before_filter :save_login_state, only: [:login, :login_attempt]
-
-  def login
+  def new
   end
 
-  def logout
-    session[:user_id] = nil
-    redirect_to action: 'login'
+  def create
+    user = User.authenticate(params[:username], params[:password])
+    sign_in(user)
   end
 
-  def login_attempt
-    authorized_user = User.authenticate(params[:username], params[:login_password])
-    if authorized_user
-      session[:user_id] = authorized_user.id
-      flash[:notice] = "Welcome again, you logged in as #{authorized_user.name}"
-      redirect_to root_url
-    else
-      flash[:notice] = "Invalid Username or Password"
-      flash[:color] = "invalid"
-      render "login"
-    end
+  def destroy
+    reset_session
+    redirect_to root_path
   end
 end
