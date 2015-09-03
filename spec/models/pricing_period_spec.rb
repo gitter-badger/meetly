dbb15989ed5088f33f72c48d2934ced28340be52
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-09-03 22:15:55 +0200

    [#2] Implement pending validation specs for Pricing Period Model

--- spec/models/pricing_period_spec.rb
+++ spec/models/pricing_period_spec.rb
@@ -10,12 +10,20 @@ RSpec.describe PricingPeriod, type: :model do
       expect(FactoryGirl.create(:pricing_period)).to be_valid
     end
 
-    it 'is not valid without start date'
+    it 'is not valid without start date' do
+      expect(FactoryGirl.build(:pricing_period, start_date: nil)).to_not be_valid
+    end
 
-    it 'is not valid without end_date'
+    it 'is not valid without end_date' do
+      expect(FactoryGirl.build(:pricing_period, end_date: nil)).to_not be_valid
+    end
 
-    it 'is not valid without event'
+    it 'is not valid without event' do
+      expect(FactoryGirl.build(:pricing_period, event_id: nil)).to_not be_valid
+    end
 
-    it 'is not valid without name'
+    it 'is not valid without name' do
+      expect(FactoryGirl.build(:pricing_period, name: nil)).to_not be_valid
+    end
   end
 end
