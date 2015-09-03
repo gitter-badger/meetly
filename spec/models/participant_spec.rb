a5bd4ec35f79483df5b17f98cbcc26c2d2a6251b
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-09-03 22:19:56 +0200

    [#2] Implement pending validation specs for Participant Model

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -14,25 +14,46 @@ describe Participant, type: :model do
       expect(FactoryGirl.build(:participant, first_name: nil)).to_not be_valid
     end
 
-    it 'is invalid without last name'
+    it 'is invalid without last name' do
+      expect(FactoryGirl.build(:participant, last_name: nil)).to_not be_valid
+    end
 
-    it 'is invalid without a gender'
+    it 'is invalid without a gender' do
+      expect(FactoryGirl.build(:participant, gender: nil)).to_not be_valid
+    end
 
-    it 'is invalid without an email'
+    it 'is invalid without an email' do
+      expect(FactoryGirl.build(:participant, email: nil)).to_not be_valid
+    end
 
-    it 'is invalid without an age'
+    it 'is invalid without an age' do
+      expect(FactoryGirl.build(:participant, age: nil)).to_not be_valid
+    end
 
-    it 'validates numericality of age'
+    it 'validates numericality of age' do
+      expect(FactoryGirl.build(:participant, age: 'I am 23')).to_not be_valid
+      expect(FactoryGirl.build(:participant, age: 'age')).to_not be_valid
+    end
 
-    it 'is invalid without a city'
+    it 'is invalid without a city' do
+      expect(FactoryGirl.build(:participant, city: nil)).to_not be_valid
+    end
 
-    it 'is invalid without phone'
+    it 'is invalid without phone' do
+      expect(FactoryGirl.build(:participant, phone: nil)).to_not be_valid
+    end
 
-    it 'is invalid without role'
+    it 'is invalid without role' do
+      expect(FactoryGirl.build(:participant, role_id: nil)).to_not be_valid
+    end
 
-    it 'is invalid without status'
+    it 'is invalid without status' do
+      expect(FactoryGirl.build(:participant, status: nil)).to_not be_valid
+    end
 
-    it 'is invalid without event'
+    it 'is invalid without event' do
+      expect(FactoryGirl.build(:participant, event_id: nil)).to_not be_valid
+    end
 
     it 'calculates expected cost'
 
