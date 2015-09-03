9be46e9368c433ba3b77279cb9e513819de6541e
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-09-03 22:13:54 +0200

    [#2] Implement pending specs for Role Model

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -10,10 +10,17 @@ describe Role do
       expect(FactoryGirl.create(:role)).to be_valid
     end
 
-    it 'is not valid without event'
+    it 'is not valid without event' do
+      expect(FactoryGirl.build(:role, event_id: nil)).to_not be_valid
+    end
 
-    it 'is not valid without name'
+    it 'is not valid without name' do
+      expect(FactoryGirl.build(:role, name: nil)).to_not be_valid
+    end
 
-    it 'validates uniqueness of name for event'
+    it 'validates uniqueness of name for event' do
+      FactoryGirl.create(:role, name: 'identical', event_id: '1')
+      expect(FactoryGirl.build(:role, name: 'identical', event_id: '1')).to_not be_valid
+    end
   end
 end
