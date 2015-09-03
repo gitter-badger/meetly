d962d4d33954ca43445dd241fb06cd410fc848b4
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-09-03 22:02:50 +0200

    [#2] Implement pending specs for Service Group Model

--- spec/models/service_group_spec.rb
+++ spec/models/service_group_spec.rb
@@ -6,8 +6,14 @@ RSpec.describe ServiceGroup, type: :model do
       expect(FactoryGirl.create(:service_group)).to be_valid
     end
 
-    it 'is not valid without name'
+    it 'is not valid without name' do
+      expect(FactoryGirl.build(:service_group, name: nil)).to_not be_valid
+    end
 
-    it 'validates uniqueness of name'
+    it 'validates uniqueness of name' do
+      FactoryGirl.create(:service_group, name: 'identical')
+
+      expect(FactoryGirl.build(:service_group, name: 'identical')).to_not be_valid
+    end
   end
 end
