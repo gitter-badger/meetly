0d81cd2243dd652c635d21ecb9f4be7db0f10c25
Bartek Szczepanski <hello@barnski.pl>
2015-09-21 21:23:40 +0200

    Clear routes. Adjust request specs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,21 @@
 Rails.application.routes.draw do
+  
+  # TODO: review these resources and uncomment them
+  # resources :participants do
+  #   patch :edit_payment, on: :member, defaults: {format: 'js'}
+  #   post :payment_confirm, on: :member
+  #   delete :wipe, on: :member
+  #   patch :unarchive, on: :member
+  #   get :resend_confirmation, on: :member
+  #   put :edit_form, on: :member
+  #   get :list_mail, on: :member
+  #   patch :edit, on: :member
+  #   delete :destroy_and_mail, on: :member
+  #   patch :set_arrived, on: :member, defaults: {format: 'js'}
+  # end
 
-  resources :participants do
-    patch :edit_payment, on: :member, defaults: {format: 'js'}
-    post :payment_confirm, on: :member
-    delete :wipe, on: :member
-    patch :unarchive, on: :member
-    get :resend_confirmation, on: :member
-    put :edit_form, on: :member
-    get :list_mail, on: :member
-    patch :edit, on: :member
-    delete :destroy_and_mail, on: :member
-    patch :set_arrived, on: :member, defaults: {format: 'js'}
-  end
-
-  get '/summary', to: 'participants#summary'
-  match '/receive_form' => 'participants#receive_form', via: :post, defaults: {format: 'json'}
-  match '/receive_form' => 'participants#receive_form', via: :get, defaults: {format: 'json'}
-  match '/role_price_table' => 'roles#role_price_table', via: :get, defaults: {format: 'json'}
-
+  get '/participant_form' => 'events#form_data', defaults: { format: 'json'}
   post '/participant_form' => 'participants#receive_form', defaults: { format: 'json' }
-
-  match '/form_data' => 'events#form_data', via: :get, defaults: { format: 'json' }
-  get '/archived', to: 'participants#show_archived'
 
   get 'login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create', as: :session
--- spec/requests/participant_form_requests_spec.rb
+++ spec/requests/participant_form_requests_spec.rb
@@ -2,10 +2,37 @@ require 'rails_helper'
 require 'output_helper'
 require 'request_helper'
 
-RSpec.describe "Registration form", type: :request do
+RSpec.describe "participant_form requests", type: :request do
   before(:each) do
     OutputHelper.capture_stdout { load "#{Rails.root}/db/seeds.rb" }
   end
+
+  it 'responds with form_data on get' do
+    get '/participant_form', event: 'Poczatek', role: 'Uczestnik', format: :json 
+
+    expect(response).to be_success
+    json = JSON.parse(response.body)
+    expect(json['event_price']).to eq('140.0')
+    expect(json['services'].length).to eq(2)
+    expect(json['days'].length).to eq(3)
+
+    expect(json['days'][0]['price']).to eq('40.0')
+    expect(json['days'][1]['price']).to eq('60.0')
+    expect(json['days'][2]['price']).to eq('60.0')
+    expect(json['services'][0]['price']).to eq('10.0')
+    expect(json['services'][1]['price']).to eq('10.0')
+  end
+
+  it 'responds with not found if event not found on get' do
+    get '/participant_form', event: 'Pcztk', role: 'Uczestnik', format: :json 
+    expect(response).to have_http_status(:not_found)
+  end
+
+  it 'responds with not found if role not found on get' do
+    get '/participant_form', event: 'Poczatek', role: 'Uczstk', format: :json 
+    expect(response).to have_http_status(:not_found)
+  end
+
 
   it "responds with created and creates participant on valid participant request"
 
