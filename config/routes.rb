ba34c8a911871143f743ecead17a8f0be8e2734c
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-10-31 00:45:41 +0100

    Changee logout route to respond to DELETE request

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create', as: :session
-  get 'logout', to: 'sessions#destroy', as: :logout
+  delete 'logout', to: 'sessions#destroy', as: :logout
 
   root to: 'application#redirect_to_events_list'
 end
