cd9677d90bdb4b665249116fbf1e69b2d8f6a736
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-12-19 23:18:07 +0100

    Add loading spinner animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,3 +47,15 @@ ready = function() {
 $(document).ready(ready);
 $(document).on('page:load', ready);
 
+$(document).on('page:fetch', function() {
+  $(".loading-mask").fadeIn( "fast", function() {
+    $(".loading-mask").show();
+  })
+});
+
+$(document).on('page:change', function() {
+  setTimeout(function(){  $(".loading-mask").fadeOut( "fast", function() {
+    $(".loading-mask").hide();
+  }) }, 1500);
+});
+
