aeaf2a5e5cffe57e02026c26f59b4e5505513f3b
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-12-20 23:19:15 +0100

    Expose participants and participants_with_days_and_services separately

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,4 @@
 class DashboardController < ApplicationController
-  expose(:participants) { current_event.participants.active.includes(:days, :services) }
+  expose(:participants_with_days_and_services) { current_event.participants.active.includes(:days, :services) }
+  expose(:participants) { current_event.participants.active }
 end
