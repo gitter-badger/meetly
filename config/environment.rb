b932327dd4a954eac6bf3a4b1193389c3a4692c7
Bartek Szczepanski <hello@barnski.pl>
2016-01-22 00:31:56 +0100

    Add heroku log entry configuration

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,3 @@ load(app_environment_variables) if File.exists?(app_environment_variables)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.logger = Le.new('583d8a3c-5c1d-4eb1-8db4-17ac7d3631da', :debug => true, :local => true)
